6d74789c6e7142a02a78ff64a047089c3a6adf22
DisRachik <radchenkoig@gmail.com>
2023-12-17 17:54:42 +0200

    typification for useRef

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { PropsInput } from './TextInput.types';
 
 export function TextInput({ initialValue, onSave }: PropsInput) {
+  /*-----  -----*/
+  /*----- typification for UseState on the example of input -----*/
+  /*-----  -----*/
   const [value, setValue] = useState(initialValue);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -13,10 +16,20 @@ export function TextInput({ initialValue, onSave }: PropsInput) {
     console.log(value);
   };
 
+  /*-----  -----*/
+  /*----- typification for UseRef -----*/
+  /*-----  -----*/
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  const onBtnClickForRef = () => {
+    inputEl.current?.focus();
+  };
+
   return (
     <div>
-      <input type="text" value={value} onChange={handleChange} />
+      <input ref={inputEl} type="text" value={value} onChange={handleChange} />
       <button onClick={handleSave}>Save</button>
+      <button onClick={onBtnClickForRef}>Set input focus</button>
     </div>
   );
 }
